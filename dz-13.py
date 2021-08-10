3c9a3c55ba26cd455032176bdbfb0152db2f5ac7
Orientir <ya.orient@gmail.com>
2021-08-10 18:55:35 +0300

    dz 13 part 2

--- dz-13.py
+++ dz-13.py
@@ -1,7 +1,30 @@
 import os
 import gzip
 
-from filter import line_filter
+from filter import line_filter, line_filter_error
+
+
+def check_error_log(file_path):
+    if file_path.endswith('.gz'):
+        log_file = gzip.open(file_path)
+    else:
+        log_file = open(file_path)
+
+    for line in log_file:
+        data = line_filter_error(line)
+
+        if not data:
+            continue
+
+        date, url, status = data
+
+        key = f"{date}\t{url}\t{status}"
+        if key not in error_result:
+            error_result[key] = 1
+        else:
+            error_result[key] += 1
+
+    log_file.close()
 
 
 base_dir = 'logs2'
@@ -17,8 +40,12 @@ result_file.write('Date\tGoogleRequestCount\n')
 google_bot_file = open('google_bot_file.csv', 'w', encoding='utf-8')
 google_bot_file.write('Date\tUrl\tGoogleBotUserAgent\tCountRequest\n')
 
+error_result_file = open('error_result_file.csv', 'w', encoding='utf-8')
+error_result_file.write('Date\tUrl\tStatusCode\n')
+
 result = dict()
 google_result = dict()
+error_result = dict()
 
 
 for filename in all_log_files:
@@ -26,6 +53,7 @@ for filename in all_log_files:
     file_path = f'{base_dir}/{filename}'
 
     if 'error' in file_path:
+        check_error_log(file_path)
         continue
 
     if file_path.endswith('.gz'):
@@ -64,6 +92,11 @@ for key, val in result.items():
 for key, val in google_result.items():
     google_bot_file.write(f'{key}\t{val}\n')
 
+for key in error_result.keys():
+    error_result_file.write(f'{key}\n')
+
 result_file.close()
 google_bot_file.close()
+
+print("Files are ready")
 
--- filter.py
+++ filter.py
@@ -11,10 +11,28 @@ def line_filter(line):
         date = line.split()[3][1:].split(':')[0]
 
     except Exception:
-        #print('[Error in line]', line)
+        print('[Error in line]', line)
         return
 
     if 'google' not in user_agent.lower():
         return
 
     return ip, user_agent, url, date
+
+
+def line_filter_error(line):
+
+    if type(line) is bytes:
+        line = line.decode()
+
+    try:
+        status = line.split()[2]
+        url = line.split()[6] if 'error' in status else line.split()[-4]
+        date = line.split()[0]
+
+
+    except Exception:
+        print('[Error in line]', line)
+        return
+
+    return date, url, status
