eebbe163536227be1fec42e3b216b313869ff2ac
Orientir <ya.orient@gmail.com>
2021-09-18 10:01:01 +0300

    18-09-2021

--- dz-17.py
+++ dz-17.py
@@ -11,12 +11,16 @@ class SiteCrawler:
     file_crawled_links = "file_crawled_links.txt"
     file_to_crawl_links = "file_to_crawl_links.txt"
 
-    def __init__(self, domain, is_new):
+    def __init__(self, domain, is_new=True):
         self.domain = domain
         self.main_link = f'http://{domain}/'
         self.links_to_crawl.add(self.main_link)
         self.robots = self.get_robots()
         self.is_new = is_new
+
+        if not is_new:
+            self.read_file_crawled_links()
+            self.read_links_to_crawl()
 
         print("It was init")
         time.sleep(1)
@@ -33,22 +37,20 @@ class SiteCrawler:
             f_cr = f.readlines()
             self.crawled_links = set(x.strip() for x in f_cr)
         print("read_file_crawled_links")
-        time.sleep(1)
 
     def read_links_to_crawl(self):
         with open(self.file_to_crawl_links, 'r', encoding='utf-8') as f:
             f_cr = f.readlines()
             self.links_to_crawl = set(x.strip() for x in f_cr)
         print("read_links_to_crawl")
-        time.sleep(1)
 
-    def save_file_crawled_links(self):
-        with open(self.file_crawled_links, 'w', encoding='utf-8') as f:
+    def save_file_crawled_links(self, file):
+        print(len(self.crawled_links), '  - lenght')
+        with open(file, 'w', encoding='utf-8') as f:
             for link in self.crawled_links:
                 f.write(link+'\n')
                 f.flush()
         print("save_file_crawled_links")
-        time.sleep(1)
 
     def save_links_to_crawl(self):
         with open(self.file_to_crawl_links, 'w', encoding='utf-8') as f:
@@ -56,41 +58,45 @@ class SiteCrawler:
                 f.write(link+'\n')
                 f.flush()
         print("save_links_to_crawl")
-        time.sleep(1)
+
+
+    def start_parse(self):
+        while True:
+            try:
+                print("start parse")
+                time.sleep(1)
+                if len(self.links_to_crawl) == 0:
+                    break
+                else:
+                    print('PARSE')
+                    self.parse()
+            except:
+                time.sleep(1)
+                self.save_file_crawled_links(self.file_crawled_links)
+                self.save_links_to_crawl()
+                break
+
+            else:
+                self.save_file_crawled_links(self.ready_links)
+
+            print('end parse')
 
     def parse(self):
-        print("parse")
-        time.sleep(1)
-        try:
-            if len(self.links_to_crawl) == 0:
-                pass
-            else:
-                url = self.links_to_crawl.pop()
-                response = self.session.get(url)
-                self.crawled_links.add(url)
-                print(url)
-                for link in response.html.absolute_links:
-
-                    if self.domain not in link:
-                        continue
-                    if not self.robots.allowed(link, '*'):
-                        continue
-                    if link in self.crawled_links:
-                        continue
-                    self.links_to_crawl.add(link)
-                if self.is_new:
-                    file = open(self.ready_links, 'w', encoding="utf-8")
-                else:
-                    file = open(self.ready_links, 'a', encoding="utf-8")
-                file.write(url + '\n')
-                file.flush()
-
-        except:
-            print("parse except")
-            time.sleep(1)
-            self.save_file_crawled_links()
-            self.save_links_to_crawl()
+        url = self.links_to_crawl.pop()
+        response = self.session.get(url)
+        self.crawled_links.add(url)
+        for link in response.html.absolute_links:
+            if self.domain not in link:
+                continue
+            if not self.robots.allowed(link, '*'):
+                continue
+            if link in self.crawled_links:
+                continue
+            self.links_to_crawl.add(link)
+        print(len(self.links_to_crawl), ' -len')
 
 
-site = SiteCrawler('b-options.com', True)
-site.parse()
+
+domen = input('input domen: ')
+site = SiteCrawler(domen, False)
+site.start_parse()
